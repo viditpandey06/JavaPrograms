fbd066cab2b1a9a3bcdf079fc1bbe2432ebd4fd2
[Vidit Pandey] <viditpandey06@gmail.com>
2024-02-13 22:19:12 +0530

    Create SlidingWindow.java

---
+++ SlidingWindow.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SlidingWindow{
+    public static void main(String [] args){
+        String str="";
+        Scanner sc= new Scanner(System.in);
+        Sysout.out.println("Enter the string");
+        str= sc.nextLine();
+       String currentString=str.substring(0,4);
+       for(int i=4;i<str.length()-4;i++)
+       {
+             
+    }
+       }
+    }
