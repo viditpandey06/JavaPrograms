a325fabcea3d95a043ad26ab4cb54b2fbef91ba2
[Vidit Pandey] <viditpandey06@gmail.com>
2024-04-17 21:14:24 +0530

    Create JustCheck.java

---
+++ JustCheck.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class JustCheck {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (int i =0; i<10; i++){
+            System.out.println("Hello World");
+
+        }
+    }
+}
