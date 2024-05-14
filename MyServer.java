35d30a99174c0472e4427c8fd246f42982b4806c
viditpandey06 <viditpandey06@gmail.com>
2024-05-14 12:34:38 +0530

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -7,7 +7,7 @@ import java.net.*;
 public class MyServer {
 
     public static void main(String[] args) throws Exception {
-        ServerSocket ss = new ServerSocket(3333);// used in server program requires one parameter
+        ServerSocket ss = new ServerSocket(3333);
         Socket s = ss.accept();
         DataInputStream din = new DataInputStream(s.getInputStream());
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
